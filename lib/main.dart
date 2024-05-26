e5063d3fbc460c800289dbcbe12dfbeb731de932
Sriramkarthikakula <92425111+Sriramkarthikakula@users.noreply.github.com>
2024-05-26 11:04:13 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
-import 'package:attendance/screens/Faculty_History.dart';
+import 'package:attendance/screens/faculty_main_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:attendance/screens/loginpage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -13,27 +14,30 @@ void main() async {
   // Initialize Firebase
   await Firebase.initializeApp(
     options: FirebaseOptions(
-      apiKey: 'Add your apikey',
-      appId: 'Add your appId',
-      messagingSenderId: 'Add your messagingSenderId',
-      projectId: 'Add your projectId',
+      apiKey: 'AIzaSyCeMmJDX1d2rM7lqvZTpraRI4CI1y4PY4k',
+      appId: '1:91348246199:android:882747af209aacc67e7732',
+      messagingSenderId: '91348246199',
+      projectId: 'attendance-e35d5',
     ),
   );
   WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-  runApp(const MyApp());
+  runApp(MyApp(isLoggedIn: isLoggedIn));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isLoggedIn;
+  MyApp({Key? key, required this.isLoggedIn}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      home: isLoggedIn ? Faculty_main() : LoginPage(),
     );
   }
 }
